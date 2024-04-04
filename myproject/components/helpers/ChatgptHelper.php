973575eec8618681142ecc2a96554e0831d7cffc
TummalaDeekshitha <deekshithareddy2223@gmail.com>
2024-04-04 18:50:43 +0530

    deek3commit

--- myproject/components/helpers/ChatgptHelper.php
+++ myproject/components/helpers/ChatgptHelper.php
@@ -23,7 +23,7 @@ class ChatgptHelper extends CComponent {
     );
  
     // Get the Bearer Token
-    $authorization_token = $_ENV["openAIKey"];
+   
     // Set the Content Type
     $content_type = "application/json";
  
